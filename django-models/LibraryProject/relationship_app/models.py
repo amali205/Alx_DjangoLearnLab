3c71a785f2097436c36cc8be66ba51210759b072
am8899 <amali20253@gmail.com>
2025-08-01 03:33:21 +0300

    gg

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,7 +6,9 @@ class Author(models.Model):
   name =  models.CharField(max_length=30)
   def __str__(self):
     return self.name
-class Book(models.Mode):
+
+
+class Book(models.Model):
  title=models.CharField(max_length= 50)
  author=models.ForeignKey (Author , on_delete=models.CASCADE)
  def __str__(self):
