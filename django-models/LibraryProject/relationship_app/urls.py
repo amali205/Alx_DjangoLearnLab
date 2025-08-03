8862b99ad38b41c450ff7cf20e6830abf73ff8ae
am8899 <amali20253@gmail.com>
2025-08-03 16:37:17 +0300

    gg

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from .views import   LibraryDetailView
 from .views import list_books
 from .views import BookListView
+from . import views
 
 
 urlpatterns = [
        path('book_list1', BookListView.as_view(), name='book-listview'),
        path('book_list2', LibraryDetailView.as_view(), name='book-detail'),
        path('book_list3', list_books, name='book-list'),
+       path('register' , views.register , name= 'register'),
+       path('login', views.loginform , name='login'),
+       path('logout', views.logoutform , name='logout')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView 
 from .models import Book , Librarian , Author
 from .models import Library
+from django.contrib.auth import  logout
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 # Create your views here.
 
 
@@ -16,3 +18,13 @@ class BookListView(ListView):
 class LibraryDetailView(DetailView):
  def get(self, request):
         return render(request, 'relationship_app/library_detail.html' , {"books":Library.objects.all()})
+
+def register(request):
+    form = UserCreationForm()
+    return render(request , 'relationship_app/register.html' , {"form":form})
+def loginform(request):
+    login_form = AuthenticationForm()    
+    return render (request , 'relationship_app/login.html' ,  {"form": login_form})
+def logoutform(request):
+    logout(request)
+    return render(request , 'relationship_app/logout.html' )
