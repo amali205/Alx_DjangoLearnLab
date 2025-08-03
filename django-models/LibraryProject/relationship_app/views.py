c20e010ee4af4da2f8bbf54d6dd704267bcb6d86
am8899 <amali20253@gmail.com>
2025-08-03 16:39:52 +0300

    gg

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,9 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView 
 from .models import Book , Librarian , Author
 from .models import Library
-from django.contrib.auth import  logout 
-from django.contrib.auth import  login
+from django.contrib.auth import logout 
+from django.contrib.auth import login
+
 from django.contrib.auth.forms import UserCreationForm 
 # Create your views here.
 
