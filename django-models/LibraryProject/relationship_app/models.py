443ad25f8952658b136f5040fcf68a4b3b38034a
am8899 <amali20253@gmail.com>
2025-08-04 15:49:29 +0300

    FF

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,9 +12,19 @@ class Author(models.Model):
 class Book(models.Model):
  title=models.CharField(max_length= 50)
  author=models.ForeignKey (Author , on_delete=models.CASCADE , related_name = 'author')
+
+  
+ class Meta:
+    permissions = [
+      ("can_add_book", "Can add book"),
+      ("can_change_book", "Can change book"),        
+      ("can_delete_book", "Can delete book"),
+    ]
+   
  def __str__(self):
      return self.title
- 
+
+
 class Library(models.Model):
   name= models.CharField(max_length= 30)
   books= models.ManyToManyField(Book)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect , get_object_or_404
 from django.views.generic.list import ListView 
 from django.views.generic.detail import DetailView 
 from .models import Book , Librarian , Author , UserProfile
@@ -53,16 +53,61 @@ def is_librarian(user):
 
 # Views
 @user_passes_test(is_admin, login_url='login')
-@login_required
+
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_member, login_url='login')
-@login_required
+
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
 @user_passes_test(is_librarian, login_url='login')
-@login_required
+
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
+
+
+
+
+@permission_required('relationship_app.can_add_book')
+@login_required
+def add_book_view(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author_id = request.POST.get('author')
+        published_year = request.POST.get('published_year')
+
+        if title and author_id and published_year:
+            try:
+                author = Author.objects.get(id=author_id)
+                Book.objects.create(title=title, author=author, published_year=published_year)
+                return redirect('list_books')
+            except Author.DoesNotExist:
+                pass
+
+    authors = Author.objects.all()
+    return render(request, 'relationship_app/add_book.html', {'authors': authors})
+
+@permission_required('relationship_app.can_change_book')
+@login_required
+def edit_book_view(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        published_year = request.POST.get('published_year')
+        if title and published_year:
+            book.title = title
+            book.published_year = published_year
+            book.save()
+            return redirect('list_books')
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete_book')
+@login_required
+def delete_book_view(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
