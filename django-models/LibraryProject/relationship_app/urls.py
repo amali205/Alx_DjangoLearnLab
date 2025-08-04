52a1fa67a14f2d7be244975f61dcbe2de90dfb40
am8899 <amali20253@gmail.com>
2025-08-04 15:38:55 +0300

    FF

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
        path('register', views.register , name= 'register'),
        path('login', LoginView.as_view(template_name="relationship_app/login.html") , name='login'),
        path('logout',LogoutView.as_view(template_name="relationship_app/logout.html")  , name='logout'),
-       path('admin', admin_view, name='admin_view'),
-       path('member', member_view, name='member_view'),
-       path('librarian',librarian_view, name='librarian_view'),     
+       path('librarian-role/', librarian_view, name='librarian_view'),
+       path('member-role/', member_view, name='member_view'),  
 ]
