08a39de1a9058461fc4fa4681f0aec42492e1dd2
am8899 <amali20253@gmail.com>
2025-08-04 09:06:12 +0300

    dd

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -35,23 +35,7 @@ class LogoutView(ListView):
       return render(request , 'logout.html' )
 
 
-def is_admin(user):
-    return user.is_authenticated and user.role == 'Admin'
-
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app\admin_view.html')
-
-def is_member(user):
-    return user.is_authenticated and user.role == 'Member'
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app\member_view.html')
-
-def is_librarian(user):
-    return user.is_authenticated and user.role == 'Librarian'
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app\librarian_view.html')
+@user_passes_test
+def Admin(request):
+    return render (request , 'relationship_app\admin_view.html')
+    
