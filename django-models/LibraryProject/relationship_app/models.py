1528b2c01a59a159a54f2c5e7be54759c2e4cf6b
am8899 <amali20253@gmail.com>
2025-08-04 08:29:25 +0300

    dd

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
 
 class Author(models.Model):
@@ -26,3 +27,16 @@ class Librarian(models.Model):
  library= models.OneToOneField (Library, on_delete=models.CASCADE , related_name = 'library')
  def __str__(self):
    return self.name
+ 
+
+
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(User , on_delete=models.CASCADE)
+  role = models.CharField(max_length=20, choices=[('Admin','Admin'),('Librarian','Librarian'),('Member','Member')])
+  def __str__(self):
+   return self.user
+
+def CreatUserProfile(sender, instance, created, **kwargs ):
+  
+
