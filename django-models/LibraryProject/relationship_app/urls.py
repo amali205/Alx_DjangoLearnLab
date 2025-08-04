74cbe3d88569337c15a362356084f7482921f890
am8899 <amali20253@gmail.com>
2025-08-04 15:51:23 +0300

    FF

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ from .views import BookListView
 from .views import LoginView
 from .views import LogoutView
 from . import views
-from .views import admin_view , member_view , librarian_view
+from .views import admin_view , member_view , librarian_view , add_book_view, edit_book_view, delete_book_view
 
 
 urlpatterns = [
@@ -16,5 +16,9 @@ urlpatterns = [
        path('login', LoginView.as_view(template_name="relationship_app/login.html") , name='login'),
        path('logout',LogoutView.as_view(template_name="relationship_app/logout.html")  , name='logout'),
        path('librarian-role/', librarian_view, name='librarian_view'),
-       path('member-role/', member_view, name='member_view'),  
+       path('member-role/', member_view, name='member_view'),   
+       path('add_book/', add_book_view, name='add_book'),
+       path('edit_book/<int:book_id>/', edit_book_view, name='edit_book'),
+       path('delete_book/<int:book_id>/', delete_book_view, name='delete_book'),
 ]
+
