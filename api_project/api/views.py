5739e2609f8a0ef2a53b95492e03167d0b1039cc
am8899 <amali20253@gmail.com>
2025-08-05 14:59:47 +0300

    ff

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView
+from .serializers import BookSerializer
+from .models import Book
+from rest_framework.response import Response
 # Create your views here.
+
+class BookList(ListAPIView):
+       def get(self , request):
+              queryset = Book.objects.all()
+              serializer_class = BookSerializer()
+              return Response(serializer_class.data)
+       
+
+
+
+    #    def get_queryset(self):                         ####    we can use this functions for more actions we need 
+    #           return Book.objects.filter()
