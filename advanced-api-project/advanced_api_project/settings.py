574ec98ae6676e7ae04ebdd64288db7f54db2491
am8899 <amali20253@gmail.com>
2025-08-13 18:20:34 +0300

    dd

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication'
+        ],
+    'DEFAULT_PERMISSIONS_CLASSES':[
+        'rest_framework.permissions.IsAuthenticated'],
+    
+}
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from .serializers import BookSerializer , AuthorSerializer
 from .models import Book ,Author
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication 
+from rest_framework.permissions import IsAuthenticated  
 # Create your views here.
 
 
@@ -33,13 +35,24 @@ class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
+
 class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+     
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
+
+
+
 
 class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
       
 
-
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
+    
