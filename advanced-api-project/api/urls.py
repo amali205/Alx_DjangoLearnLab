aef3c89b0e96ecd4f64807995c4bba80fc88f46e
am8899 <amali20253@gmail.com>
2025-08-13 16:42:16 +0300

    Ff

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,8 +2,11 @@ from django.urls import path ,include
 from .router import router
 from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 urlpatterns = [
-    path('all_books' ,ListView.as_view() ,name='all_book'),
-    path('all_books/<int:pk>', DetailView.as_view() ,name='book'),
+    path('all_books' , ListView.as_view() ,name='all_book'),
+    path('books/<int:pk>', DetailView.as_view() ,name='book'),
+    path('books/create', CreateView.as_view(),name='creat' ),  
+    path('books/update' , UpdateView.as_view() , name= 'update'),
+    path('books/delete' ,DeleteView.as_view() , name='delete'),
     path('', include(router.urls))
 ]
 
