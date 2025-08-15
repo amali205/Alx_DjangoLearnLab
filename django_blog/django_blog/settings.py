26a13280ff635596a744652e02b28462e106fa63
am8899 <amali20253@gmail.com>
2025-08-15 20:57:12 +0300

    DD

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_db_user',          # <-- Add this
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',  
     }
 }
 
