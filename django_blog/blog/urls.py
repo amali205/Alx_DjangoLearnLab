26f24352136336a804e97bd7cbeee0ee9d88459b
am8899 <amali20253@gmail.com>
2025-08-15 23:04:50 +0300

    gg

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,9 +16,9 @@ urlpatterns = [
 
     # Posts CRUD
     path('posts/', views.PostListView.as_view(), name='posts'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
 
