9761924e27992ccbf37efc3339e705d543cc084d
am8899 <amali20253@gmail.com>
2025-09-03 18:46:55 +0300

    D

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
    path('register/', RegisterView.as_view(), name='register'),
    path('login/', loginView.as_view(), name='login'),
    path('me/', meview.as_view(), name='me'),
+   Add follow/unfollow paths here if you have corresponding views, for example:
+   path('unfollow/<int:user_id>/', UnfollowView.as_view(), name='unfollow'),
+   path('follow/<int:user_id>/', FollowView.as_view(), name='follow'),
+   path('users/', include((UserViewSet.as_view({'get': 'list'}), 'users'), namespace='users')),
 
 ]
 
