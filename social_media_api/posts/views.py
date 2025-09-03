61c02c2b5df4aa72bbbf974290753e5ad812ca36
am8899 <amali20253@gmail.com>
2025-09-03 18:44:24 +0300

    D

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets , filters
+from rest_framework import viewsets , filters , permissions
 from .models import Post, Comment   
 from .serializers import PostSerializer, CommentSerializer
 # Create your views here.
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['author__username', 'tiltle', 'content']
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # Replace the following line with your actual logic to get following users
+        following_users = user.following.all() if hasattr(user, 'following') else []
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)   
